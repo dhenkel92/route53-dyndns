5ceb6c5a71f0dbd63558c41c6eeeabd98416cfa8
Daniel Henkel <daniel@henkel.tech>
2020-03-16 06:46:41 +0100

    always log error in main function

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -2,3 +2,4 @@ mod route53_handler;
 mod types;
 
 pub use route53_handler::handle_route53;
+pub use types::*;
--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod logger;
 mod route53;
 
 use crate::config::{load_config, DomainProvider};
-use crate::handler::handle_route53;
+use crate::handler::{handle_route53, HandlerError};
 use clap_config::generate_clap_config;
 use logger::initialize_logger;
 use std::error::Error;
@@ -14,19 +14,28 @@ use std::path::Path;
 #[macro_use]
 extern crate log;
 
+fn log_generic_error(err: Box<dyn Error>) -> Box<dyn Error> {
+    error!("{:?}", err);
+    err
+}
+
+fn log_handler_error(err: HandlerError) {
+    error!("{:?}", err);
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let matches = generate_clap_config().get_matches();
-    initialize_logger(&matches)?;
+    initialize_logger(&matches).map_err(log_generic_error)?;
 
     let config_path = Path::new(matches.value_of("config-path").unwrap());
-    let configuration = load_config(config_path)?;
+    let configuration = load_config(config_path).map_err(log_generic_error)?;
 
     debug!("Configuration: {:?}", configuration);
 
     for domain in configuration.domains {
-        match domain.provider {
-            DomainProvider::Aws => handle_route53(&domain)?,
-        }
+        let _ = match domain.provider {
+            DomainProvider::Aws => handle_route53(&domain).map_err(log_handler_error),
+        };
     }
 
     Ok(())
