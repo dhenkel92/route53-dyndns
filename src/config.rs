ebe3d25b9c636b5223dc8db70dc24e930e6bc1fd
Daniel Henkel <daniel@henkel.tech>
2020-03-31 06:46:53 +0200

    implement unit tests for config module

--- src/config.rs
+++ src/config.rs
@@ -26,3 +26,28 @@ pub fn load_config(path: &Path) -> Result<Configuration, Box<dyn Error>> {
     let config: Configuration = serde_yaml::from_str(&contents)?;
     Ok(config)
 }
+
+#[cfg(test)]
+mod tests {
+    mod load_config {
+        use crate::config::load_config;
+        use std::path::Path;
+
+        #[test]
+        fn should_load_config_and_include_at_least_one_domain() {
+            let path = Path::new(file!())
+                .parent()
+                .unwrap()
+                .join("../example-config.yaml");
+            let config = load_config(path.as_path()).unwrap();
+            assert!(config.domains.len() > 0)
+        }
+
+        #[test]
+        fn should_fail_while_loading_config() {
+            let path = Path::new("invalid-path");
+            let err = load_config(path).err();
+            assert_ne!(err.unwrap().to_string(), "")
+        }
+    }
+}
