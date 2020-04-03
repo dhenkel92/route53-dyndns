fb41ab2ae1edc514859a86fd5b9e95df8876c898
Daniel Henkel <daniel@henkel.tech>
2020-04-03 17:54:27 +0200

    Revert "enable cargo incremental for code cov"
    This reverts commit e1ca2c9e7d2d80a452e6d8045496de50fde34cb7.

--- .github/scripts/create_test_bin.sh
+++ .github/scripts/create_test_bin.sh
@@ -8,6 +8,6 @@ rustup install nightly
 rustup default nightly
 
 # Compile test
-export CARGO_INCREMENTAL=1
+export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads --cfg procmacro2_semver_exempt"
 cargo test --no-run
