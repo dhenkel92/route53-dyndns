7384a30d864a37156c274e683a539f0d0084c102
Daniel Henkel <daniel@henkel.tech>
2020-06-08 07:45:50 +0200

    replace no-langing-pads with -Cpanic=abort
    https://github.com/rust-lang/rust/pull/70175

--- .github/scripts/create_test_bin.sh
+++ .github/scripts/create_test_bin.sh
@@ -9,5 +9,5 @@ rustup default nightly
 
 # Compile test
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off --cfg procmacro2_semver_exempt"
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Cpanic=abort -Coverflow-checks=off --cfg procmacro2_semver_exempt"
 cargo test --no-run
