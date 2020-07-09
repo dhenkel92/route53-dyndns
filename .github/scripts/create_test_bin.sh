0794b4f249824ddd6dc33a818d2333aee8c0a23d
Daniel Henkel <daniel@henkel.tech>
2020-07-09 10:36:55 +0200

    reenable some compiler flags

--- .github/scripts/create_test_bin.sh
+++ .github/scripts/create_test_bin.sh
@@ -4,10 +4,10 @@ set -ex
 
 # Install rust nightly
 rm -Rf ./target
-# rustup install nightly
-# rustup default nightly
+rustup install nightly
+rustup default nightly
 
 # Compile test
-# export CARGO_INCREMENTAL=0
-# export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Cpanic=abort -Zpanic_abort_tests -Coverflow-checks=off --cfg procmacro2_semver_exempt"
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Cpanic=abort -Zpanic_abort_tests -Coverflow-checks=off"
 cargo test --no-run
